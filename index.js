e82014847ad34b0d6510112fa2798e281922000f
imshuvosarker <sarker3664@gmail.com>
2022-11-26 18:06:38 +0600

    Update for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const { PORT } = process.env;
 
 app.use(express.static(`${__dirname}/assets`));
 
